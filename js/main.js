b90165801d9b8ea7c315e3c834127170ddc24851
owner08 <owwner08@gmail.com>
2024-05-07 10:22:49 +0500

    finish

--- js/main.js
+++ js/main.js
@@ -1,1 +1,65 @@
-document.querySelector(".header__burger")&&(document.querySelector(".header__burger").onclick=()=>{document.querySelector(".header__burger").classList.toggle("active"),document.querySelector(".header__menu").classList.toggle("active")}),document.querySelectorAll("[tab]").forEach(t=>{t.addEventListener("click",e=>{document.querySelectorAll(`[tab="${t.getAttribute("tab")}"]`).forEach(e=>{e.classList.remove("active")}),document.querySelectorAll(`[tab-content="${t.getAttribute("tab")}"]`).forEach(e=>{e.classList.remove("active")}),t.classList.add("active"),document.querySelector(`[tab-content="${t.getAttribute("tab")}"]#`+t.id).classList.add("active")})});const calendar=new VanillaCalendar("#calendar",{settings:{lang:"ru",visibility:{theme:"light"}}});calendar.init();
+if (document.querySelector('.header__burger')) {
+    document.querySelector('.header__burger').onclick = () => {
+        document.querySelector('.header__burger').classList.toggle('active');
+        document.querySelector('.header__menu').classList.toggle('active');
+    };
+}
+if (document.querySelectorAll('[tab]')) {
+    document.querySelectorAll('[tab]').forEach(tab => {
+        tab.addEventListener('click', (event) => {
+            document.querySelectorAll(`[tab="${tab.getAttribute('tab')}"]`).forEach(allTab => {
+                allTab.classList.remove('active');
+            });
+            document.querySelectorAll(`[tab-content="${tab.getAttribute('tab')}"]`).forEach(allTabContent => {
+                allTabContent.classList.remove('active');
+            });
+    
+            tab.classList.add('active');
+            document.querySelector(`[tab-content="${tab.getAttribute('tab')}"]#${tab.id}`).classList.add('active');
+        });
+    });
+}
+if(document.querySelector('#calendar')) {
+    const calendar = new VanillaCalendar('#calendar', {
+        settings: {
+            lang: 'ru',
+            visibility: {
+                theme: 'light',
+                weekend: false,
+                today: false,
+            },
+            selection: {
+                day: 'multiple',
+            },
+            selected: {
+                dates: ['2024-05-01', '2024-05-02'],
+            },
+        }
+        
+    });
+    calendar.init();
+}
+if (document.querySelector('#diagram')) {
+    new Chart(document.querySelector('#diagram'), {
+        type: 'bar',
+        data: {
+            labels: ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'],
+            datasets: [{
+                label: '# of Votes',
+                data: [1, 2, 1, 2, 3, 4, 4, 5, 3, 5, 2, 3],
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+    
+        }
+    }); 
+}
+document.addEventListener('DOMContentLoaded', () => {
+    if(document.querySelector('.popup__close')) {
+        document.querySelector('.popup__close').addEventListener('click', (el) => {
+            document.querySelector('.popup').classList.remove('active');
+        });
+    }
+});
