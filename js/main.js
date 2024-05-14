89f40d5cac53acae1de4909404ca8bd8f7c091d2
owner08 <owwner08@gmail.com>
2024-05-14 16:45:29 +0500

    pages admin && secretary

--- js/main.js
+++ js/main.js
@@ -4,21 +4,23 @@ if (document.querySelector('.header__burger')) {
         document.querySelector('.header__menu').classList.toggle('active');
     };
 }
-if (document.querySelectorAll('[tab]')) {
-    document.querySelectorAll('[tab]').forEach(tab => {
-        tab.addEventListener('click', (event) => {
-            document.querySelectorAll(`[tab="${tab.getAttribute('tab')}"]`).forEach(allTab => {
-                allTab.classList.remove('active');
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.querySelectorAll('[tab]')) {
+        document.querySelectorAll('[tab]').forEach(tab => {
+            tab.addEventListener('click', (event) => {
+                document.querySelectorAll(`[tab="${tab.getAttribute('tab')}"]`).forEach(allTab => {
+                    allTab.classList.remove('active');
+                });
+                document.querySelectorAll(`[tab-content="${tab.getAttribute('tab')}"]`).forEach(allTabContent => {
+                    allTabContent.classList.remove('active');
+                });
+        
+                tab.classList.add('active');
+                document.querySelector(`[tab-content="${tab.getAttribute('tab')}"]#${tab.id}`).classList.add('active');
             });
-            document.querySelectorAll(`[tab-content="${tab.getAttribute('tab')}"]`).forEach(allTabContent => {
-                allTabContent.classList.remove('active');
-            });
-    
-            tab.classList.add('active');
-            document.querySelector(`[tab-content="${tab.getAttribute('tab')}"]#${tab.id}`).classList.add('active');
         });
-    });
-}
+    }
+});
 if(document.querySelector('#calendar')) {
     const calendar = new VanillaCalendar('#calendar', {
         settings: {
@@ -40,9 +42,12 @@ if(document.querySelector('#calendar')) {
     calendar.init();
 }
 document.addEventListener('DOMContentLoaded', () => {
-    if(document.querySelector('.popup__close')) {
-        document.querySelector('.popup__close').addEventListener('click', (el) => {
-            document.querySelector('.popup').classList.remove('active');
+    if(document.querySelectorAll('#popup-close')) {
+        document.querySelectorAll('#popup-close').forEach((button) => {
+            button.addEventListener('click', (el) => {
+                document.querySelector('.popup').classList.remove('active');
+            });
         });
+        
     }
 });
